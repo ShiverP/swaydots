29160d0d3a08369a7a5355e5e5200299a5dd3515
ShiverP <shivamparwat01@gmail.com>
2024-06-03 18:58:22 +0200

    Removed Chrome

--- .config/google-chrome/Default/Extensions/knaodoefkjbgmmilogebghadhmnphjih/4.0.14_1/content.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("enhancer-watchtime",(function(e){chrome.runtime.sendMessage(e.detail,(function(e){document.dispatchEvent(new CustomEvent("enhancer-watchtime-response",{detail:e}))}))}));
-//# sourceMappingURL=content.js.map
